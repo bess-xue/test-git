30120a1b34318bd035ac4f0f1be56a583c4ef78b
bess-xue <77708763+bess-xue@users.noreply.github.com>
2022-11-20 20:03:59 +0800

    Add files via upload

---
+++ test1.java
@@ -0,0 +1,9 @@
+package com.test;
+
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("hello World!");
+
+        /Users/bess/IdeaProjects/untitled/out/test/untitled
+    }
+}
