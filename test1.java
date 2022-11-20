7fdfcfe25ba6331a23835d59fbfff07304855086
bess-xue <77708763+bess-xue@users.noreply.github.com>
2022-11-20 20:04:20 +0800

    Delete test1.java

--- test1.java
+++
@@ -1,9 +0,0 @@
-package com.test;
-
-public class test1 {
-    public static void main(String[] args) {
-        System.out.println("hello World!");
-
-        /Users/bess/IdeaProjects/untitled/out/test/untitled
-    }
-}
